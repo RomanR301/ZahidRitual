cd39e48ce4bf0d35f5a7bca820b19101a06f9541
MAKSYM <roman.sav15@gmail.com>
2020-10-22 17:50:13 +0300

    home page (info-block, services, why-us, about, prices, catalog, faq) sections

--- ZahidRitual/js/script.js
+++ ZahidRitual/js/script.js
@@ -145,14 +145,38 @@ jQuery(function () {
     
 });
 
-// $(window).scroll(function () {
-//   if ($(this).scrollTop() > 10) {
-//     $('.scroll-to-top').addClass("scrolled");
-//   } else {
-//   	$('.scroll-to-top').removeClass("scrolled");
-//   }
-// });
-
+// $(document).ready(function() {
+//     $(".accordion__item").on("click", function(e) {
+//     e.preventDefault();
+//         if ($(this).hasClass("active")) {
+//         $(this).removeClass("active");
+//         $(this)
+//             .find(".accordion__content")
+//             .slideUp(200);
+//         } else {
+//         $(".accordion__item").removeClass("active");
+//         $(this).addClass("active");
+//         $(".accordion__content").slideUp(200);
+//         $(this)
+//             .find(".accordion__content")
+//             .slideDown(200);
+//         }
+//     });
+//   });
+$(document).ready(function() {
+    $(".accordion__item .accordion__button").on("click", function(e) {
+    e.preventDefault();
+        if ($(this).parent().hasClass("active")) {
+        $(this).parent().removeClass("active");
+        $(this).parent().find(".accordion__content").slideUp(200);
+        } else {
+        $(".accordion__item").removeClass("active");
+        $(this).parent().addClass("active");
+        $(".accordion__content").slideUp(200);
+        $(this).parent().find(".accordion__content").slideDown(200);
+        }
+    });
+  });
 document.body.addEventListener('keyup', function(e) {
   if (e.which === 9) /* tab */ {
     document.body.classList.remove('no-focus-outline');
